c642b28727dc974f5d7be57942858b31616fcfbe
vaibhavjuyal1 <vaibhavjuyal69@gmail.com>
2025-11-05 23:28:24 +0530

    Initial commit - Jivaka project

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   methods: ["GET", "POST"],
   credentials: true,
 };
